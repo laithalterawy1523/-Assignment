379c4c684a3ba8afb49e0477205bbd2285a46523
odaiw <odaiw@DESKTOP-TQL3T8N>
2026-02-09 18:43:18 +0300

    Add project files.

---
+++ AssignmentOne/Program.cs
@@ -0,0 +1,12 @@
+﻿string billAmount;
+Console.Write("Please Enter The BillAmount:");
+billAmount = Console.ReadLine();
+double BillAmount = Convert.ToDouble(billAmount);
+if (BillAmount >= 500)
+    BillAmount = BillAmount - (BillAmount * .2);
+else if (BillAmount > 300 & BillAmount <= 499)
+    BillAmount = BillAmount - (BillAmount * .1);
+else
+    Console.WriteLine("No Discount Was Applied The Invoice");
+
+Console.WriteLine($"Final Price : {BillAmount}");
